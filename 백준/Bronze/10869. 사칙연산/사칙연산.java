d8385cd8b4491f13cb39359a9dc8a777cf7327a3
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-14 18:15:15 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,13 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(a + b).append("\n")
+          .append(a - b).append("\n")
+          .append(a * b).append("\n")
+          .append(a / b).append("\n")
+          .append(a % b);
+        
+        System.out.print(sb);
     }
 }
