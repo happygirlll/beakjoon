73095a69190a69dbd3f010973d5f49c84f248a8b
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-14 21:50:56 +0900

    [Bronze III] Title: 곱셈, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,29 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] num1 = new int[3];
-        int[] num2 = new int[3];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 첫 번째 세 자리 수 입력
-        String first = sc.nextLine();
-        for (int i = 0; i < 3; i++) {
-            num1[i] = first.charAt(i) - '0';
-        }
+        String num1 = br.readLine();
+        String num2 = br.readLine();
 
-        // 두 번째 세 자리 수 입력
-        String second = sc.nextLine();
-        for (int i = 0; i < 3; i++) {
-            num2[i] = second.charAt(i) - '0';
-        }
-        
-        int firstSum = (num1[2] * num2[2]) + (num1[1] * num2[2] * 10) + (num1[0] * num2[2] * 100);
-        int secondSum = (num1[2] * num2[1]) + (num1[1] * num2[1] * 10) + (num1[0] * num2[1] * 100);
-        int thirdSum = (num1[2] * num2[0]) + (num1[1] * num2[0] * 10) + (num1[0] * num2[0] * 100);
+        int a = num1.charAt(0) - '0', b = num1.charAt(1) - '0', c = num1.charAt(2) - '0';
+        int d = num2.charAt(0) - '0', e = num2.charAt(1) - '0', f = num2.charAt(2) - '0';
+
+        int firstSum = c * f + b * f * 10 + a * f * 100;
+        int secondSum = c * e + b * e * 10 + a * e * 100;
+        int thirdSum = c * d + b * d * 10 + a * d * 100;
+        int totalSum = firstSum + secondSum * 10 + thirdSum * 100;
+
         System.out.println(firstSum);
         System.out.println(secondSum);
         System.out.println(thirdSum);
-        System.out.println(firstSum + (secondSum*10) + (thirdSum*100));
+        System.out.println(totalSum);
     }
 }
