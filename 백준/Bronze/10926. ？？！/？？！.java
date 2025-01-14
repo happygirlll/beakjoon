2afdbdafa847948248a7c7ec412568c2798bcefc
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-14 18:25:16 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.println(a + "??!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String username = br.readLine(); // 아이디 입력 받기
+        System.out.println(username + "??!"); // 놀람 출력
     }
 }
