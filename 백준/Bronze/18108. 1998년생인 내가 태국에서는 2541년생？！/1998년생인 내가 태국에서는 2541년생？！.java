d9bcae164e5107ebc8d70a3f09feeceafaf30199
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-14 18:35:42 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = n - 543;
-        System.out.print(m);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 입력 값을 정수로 변환
+        System.out.print(n - 543); // 바로 계산 및 출력
     }
 }
