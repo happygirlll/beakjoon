4158bde3c2e285de3c3ed686f0550b6ca7ee09f9
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-15 15:34:53 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -5,30 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int score = sc.nextInt();
-        String grade;
+        String[] grades = {"F", "F", "F", "F", "F", "F", "D", "C", "B", "A", "A"};
 
-        switch (score / 10) {
-            case 10:
-            case 9:
-                grade = "A";
-                break;
-            case 8:
-                grade = "B";
-                break;
-            case 7:
-                grade = "C";
-                break;
-            case 6:
-                grade = "D";
-                break;
-            default:
-                grade = "F";
-                break;
+        if (score < 0 || score > 100) {
+            System.out.println("Invalid score");
+        } else {
+            System.out.println(grades[score / 10]);
         }
 
-        System.out.println(grade);
-
         sc.close();
-
     }
 }
