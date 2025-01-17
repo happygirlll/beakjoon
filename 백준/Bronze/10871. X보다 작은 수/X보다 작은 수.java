7c29ff97119ebb129bdf4ea82ac9a2eae5091948
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-17 15:12:18 +0900

    [Bronze V] Title: X보다 작은 수, Time: 144 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -5,22 +5,25 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(); // 출력 최적화를 위한 StringBuilder 사용
+        
+        // 첫 번째 입력: N과 X
         String[] input = br.readLine().split(" ");
-
         int N = Integer.parseInt(input[0]);
         int X = Integer.parseInt(input[1]);
-
-        String[] ArrayInput = br.readLine().split(" ");
-        int[] A = new int[N];
+        
+        // 두 번째 입력: 수열 A
+        String[] arrayInput = br.readLine().split(" ");
+        
+        // X보다 작은 값만 처리
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(ArrayInput[i]);
-        }
-
-        for(int j = 0; j < N; j++){
-            if(A[j] < X){
-                System.out.print(A[j]+" ");
+            int num = Integer.parseInt(arrayInput[i]);
+            if (num < X) {
+                sb.append(num).append(" "); // StringBuilder에 추가
             }
         }
-
+        
+        // 결과 출력
+        System.out.println(sb.toString().trim()); // StringBuilder에 저장된 결과 출력
     }
 }
