2a1867cde5e92c2c759d422553708a6030b70824
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-17 15:38:53 +0900

    [Bronze III] Title: 최소, 최대, Time: 460 ms, Memory: 92484 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,28 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
+        
         int N = Integer.parseInt(br.readLine());
+        
+        // 두 번째 입력: N개의 정수를 배열로 저장
         int[] arr = new int[N];
-
-        String[] input = br.readLine().split(" ");
-
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
         // 최소값과 최대값 초기화
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-
+        
+        // 배열을 순회하며 최소값과 최대값 갱신
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(input[i]);
-            if (num < min) {
-                min = num;
+            if (arr[i] < min) {
+                min = arr[i];
             }
-            if (num > max) {
-                max = num;
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
 
