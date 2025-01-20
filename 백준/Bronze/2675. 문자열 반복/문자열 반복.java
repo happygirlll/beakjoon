5dd2ceec6839a91cfe5b6860e760a2b718ad3c8e
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-01-20 15:53:43 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<T;i++){
+            String[] input = br.readLine().split(" ");
+            int R = Integer.parseInt(input[0]);
+            String S=input[1];
+
+            StringBuilder result = new StringBuilder();
+            for(int j=0;j<S.length();j++){
+                for(int k=0;k<R;k++){
+                    result.append(S.charAt(j));
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
