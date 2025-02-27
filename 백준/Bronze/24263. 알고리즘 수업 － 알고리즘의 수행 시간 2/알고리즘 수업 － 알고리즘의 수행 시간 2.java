206f7ea4b407542a20a2e42c2f52319dd1554da2
BOMI <118085573+happygirlll@users.noreply.github.com>
2025-02-27 10:49:12 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 96 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println(n); // 코드1의 실행 횟수
+        System.out.println(1); // 최고차항 차수
+    }
+}
